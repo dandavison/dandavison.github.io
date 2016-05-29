af85e599c626af222690188422435afa89df3708
Dan Davison <dandavison7@gmail.com>
2016-05-29 13:46:59 -0700

    Enable mathjax plugin
    https://github.com/barrysteyn/pelican_plugin-render_math

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,6 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['plugins']
+PLUGINS = ['render_math']
